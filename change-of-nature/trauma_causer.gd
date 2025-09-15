509ae5f0f223448756e449df7732989c7455064f
Videcraft <90282888+Videocraft10@users.noreply.github.com>
2025-09-15 13:38:39 -0400

    Updated Trauma causer
    Added a distance based system that the trauma amount changes based on origin distance of character and trauma causer. note to self: still need to bug fix the distance code on trauma cuaser

--- change-of-nature/trauma_causer.gd
+++ change-of-nature/trauma_causer.gd
@@ -2,12 +2,23 @@ extends Area3D
 
 @export var set_trauma_amount := 1.0
 @export var set_trauma_reduction := 1.0
+@export var is_distance_based := true
 
 func cause_trauma():
 	var trauma_areas := get_overlapping_areas()
 	for area in trauma_areas:
 		if area.has_method("trauma_ready") && area.trauma_ready("yes"):
-			area.add_trauma_transmit(set_trauma_amount)
+			if not is_distance_based:
+				area.add_trauma_transmit(set_trauma_amount)
+				print("Caused trauma(unscaled): ", set_trauma_amount)
+				continue
+			# Calculate distance from area to this object's origin
+			var distance = global_transform.origin.distance_to(area.global_transform.origin)
+			# Invert and scale trauma, closer = more trauma, farther = less
+			var max_distance = 10.0 
+			var scaled_trauma = set_trauma_amount * (1.0 - clamp(distance / max_distance, 0.0, 1.0))
+			area.add_trauma_transmit(scaled_trauma)
+			print("Caused trauma(distance): ", scaled_trauma)
 			
 func trauma_reduction_rate():
 	var trauma_areas := get_overlapping_areas()
