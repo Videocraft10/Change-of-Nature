37f6409a5f836adfd0d60540c491b5f01ae90177
Videocraft10 <90282888+Videocraft10@users.noreply.github.com>
2025-09-27 20:34:37 -0400

    Changed some door behaviors

--- change-of-nature/blueprints/scripts/BP_small_door.gd
+++ change-of-nature/blueprints/scripts/BP_small_door.gd
@@ -1,15 +1,20 @@
 extends Area3D
+var is_open: bool = false
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		var anim_player = get_node("SmallDoor/AnimationPlayer")
-		anim_player.play("ArmatureAction")
-		$StaticBody3D/CollisionShape3D.set_deferred("disabled", true)
-		print("Door Opened")
+	if is_open:
+		return
+	else:
+		if body.is_in_group("player"):
+			var anim_player = get_node("SmallDoor/AnimationPlayer")
+			anim_player.play("ArmatureAction")
+			$StaticBody3D/CollisionShape3D.set_deferred("disabled", true)
+			print("Door Opened")
+			is_open = true
 		
-func _on_body_exited(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		var anim_player = get_node("SmallDoor/AnimationPlayer")
-		anim_player.play_backwards("ArmatureAction")
-		$StaticBody3D/CollisionShape3D.set_deferred("disabled", false)
-		print("Door Closed")
+#func _on_body_exited(body: Node3D) -> void:
+	#if body.is_in_group("player"):
+		#var anim_player = get_node("SmallDoor/AnimationPlayer")
+		#anim_player.play_backwards("ArmatureAction")
+		#$StaticBody3D/CollisionShape3D.set_deferred("disabled", false)
+		#print("Door Closed")
